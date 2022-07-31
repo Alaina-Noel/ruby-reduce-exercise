79dd2592276de11ed05f7983c2c04a9e15d1e676
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-31 09:40:55 -0600

    Complete my work

--- lib/reducer.rb
+++ lib/reducer.rb
@@ -1,1 +1,16 @@
+class Reducer
+  attr_reader :array
 
+  def initialize(array)
+  @array = array
+  end
+
+  def reduce
+    array.tally { |num| num.count }
+  end
+
+  def reduce2
+    array.tally { |num| num.count }
+  end
+
+end
--- test/reduce_test.rb
+++ test/reduce_test.rb
@@ -7,19 +7,21 @@ describe Reducer do
   it "can reduce to a hash of how many times the number occurs in the array." do
     number_array = [1,1,2,2,2,2,3,3,4]
     reducer = Reducer.new(number_array)
-    assert_equal ({1 => 2, 2 => 4, 3 => 2, 4 => 1}), reducer.reduce
+    expect(reducer.reduce).to eq({1 => 2, 2 => 4, 3 => 2, 4 => 1})
   end
 
   it "can reduce an array of letters to a hash of letter frequency" do
-    skip
     letter_array = ["a", "b", "c", "c", "d", "d", "d"]
-    assert_equal ({"a"=>1, "b" => 1, "c" => 2, "d" => 3}), reducer.reduce
+    reducer = Reducer.new(letter_array)
+
+    expect(reducer.reduce2).to eq({"a"=>1, "b" => 1, "c" => 2, "d" => 3})
   end
 
   it "can reverse a sentence with reduce" do
-    skip
     sentence = "I am a unicorn"
     reducer = Reducer.new(sentence)
+    expect(reducer.reduce3).to eq("nrocinu a ma I")
+
     assert_equal "nrocinu a ma I", reducer.sent_reverse
   end
 
